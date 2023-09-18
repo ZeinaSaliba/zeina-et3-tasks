cdfac69f14386878f1008672dd886e6ac0087394
zeinasaliba23 <zeina.saliba@et3arraf.com>
2023-09-18 14:10:53 +0300

    rempved spread operators

--- ToDo/app/screens/TodoList.js
+++ ToDo/app/screens/TodoList.js
@@ -15,7 +15,7 @@ export default function ToDo() {
     if (newTask) {
       // Create a new task object with a unique ID, creation date, title, and description
       const task = {
-        id: tasks.length + 1,
+        id: tasks?.length + 1,
         created_on: new Date().toLocaleString(),
         taskTitle: newTask,
         taskDescription: description, // Use the description input
@@ -35,7 +35,7 @@ export default function ToDo() {
 
   const removeTask = (taskId) => {
     // Filter the tasks array to keep all tasks except the one with the given ID
-    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    const updatedTasks = tasks.filter((task) => task?.id !== taskId);
 
     // Update the tasks array with the filtered array
     setTasks(updatedTasks);
@@ -43,36 +43,37 @@ export default function ToDo() {
 
   const editTask = (task) => {
     // Set the editingIndex to the ID of the task being edited
-    setEditingIndex(task.id);
+    setEditingIndex(task?.id);
 
     // Populate the input fields with the text of the task
-    setNewTask(task.taskTitle);
-    setDescription(task.taskDescription);
+    setNewTask(task?.taskTitle);
+    setDescription(task?.taskDescription);
   };
 
   const updateTask = () => {
     if (editingIndex !== -1 && newTask) {
-      // Create a copy of the tasks array
-      const updatedTasks = [...tasks];
-  
       // Find the index of the task to update by ID
-      const indexToUpdate = updatedTasks.findIndex((task) => task.id === editingIndex);
+      const indexToUpdate = tasks.findIndex((task) => task.id === editingIndex);
   
       if (indexToUpdate !== -1) {
+        // Create a new array to hold the updated tasks
+        const updatedTasks = tasks.slice();
+  
         // Update the properties of the task directly
         updatedTasks[indexToUpdate].taskTitle = newTask;
         updatedTasks[indexToUpdate].taskDescription = description;
+  
+        // Update the tasks array with the edited task
+        setTasks(updatedTasks);
+  
+        // Clear the input fields and reset editingIndex
+        setNewTask('');
+        setDescription('');
+        setEditingIndex(-1);
       }
-  
-      // Update the tasks array with the edited task
-      setTasks(updatedTasks);
-  
-      // Clear the input fields and reset editingIndex
-      setNewTask('');
-      setDescription('');
-      setEditingIndex(-1);
     }
   };
+  
   
 
   return (
@@ -94,13 +95,13 @@ export default function ToDo() {
 
       )}
 
-      {tasks.map((task) => (
+      {tasks.map((task, key) => (
         <View
-          key={task.id}
+          key={key}
           style={{ flexDirection: 'row', alignItems: 'center' }}
         >
-          <Text>{task.taskTitle}</Text>
-          <Text>{task.taskDescription}</Text>
+          <Text>{task?.taskTitle}</Text>
+          <Text>{task?.taskDescription}</Text>
           <Edit editTask={()=>{editTask(task)}}/>
           <Remove removeTask={()=>{removeTask(task.id)}} />
         </View>
