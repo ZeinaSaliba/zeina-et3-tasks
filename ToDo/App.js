3cfd5d17289195974707269faf01922f8e00af30
zeinasaliba23 <zeina.saliba@et3arraf.com>
2023-09-20 13:42:51 +0300

    apply latest stash

---
+++ ToDo/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ToDo from './app/screens/TodoList'; 
+
+export default function App() {
+  return (
+    <ToDo></ToDo>
+    );
+}
--- ToDo/index.js
+++ ToDo/index.js
@@ -3,8 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-// import App from './App';
-import ToDoList from './app/screens/TodoList';
+import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => ToDoList);
+AppRegistry.registerComponent(appName, () => App);
